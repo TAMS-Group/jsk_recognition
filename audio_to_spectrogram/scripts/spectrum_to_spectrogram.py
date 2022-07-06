a1933b2403e2071b3b5ebb976b9ca8e1891d680f
v4hn <me@v4hn.de>
2022-07-06 21:10:47 +0200

    spectrogram: lock buffer properly

--- audio_to_spectrogram/scripts/spectrum_to_spectrogram.py
+++ audio_to_spectrogram/scripts/spectrum_to_spectrogram.py
@@ -4,6 +4,7 @@ import cv2
 from cv_bridge import CvBridge
 import numpy as np
 import rospy
+from threading import Lock
 
 from jsk_recognition_msgs.msg import Spectrum
 from sensor_msgs.msg import Image
@@ -23,6 +24,7 @@ class SpectrumToSpectrogram(object):
         spectrum_msg = rospy.wait_for_message('~spectrum', Spectrum)
         spectrum_len = len(spectrum_msg.amplitude)
         # Buffer for spectrum topic
+        self.spectrum_lock = Lock()
         self.spectrogram = np.zeros((0, spectrum_len), dtype=np.float32)
         self.spectrum_stamp = []
         # Period[s] to store audio data to create one spectrogram topic
@@ -41,33 +43,35 @@ class SpectrumToSpectrogram(object):
         self.bridge = CvBridge()
 
     def audio_cb(self, msg):
-        # Add spectrum msg to buffer
-        spectrum = np.array(msg.amplitude, dtype=np.float32)
-        self.spectrogram = np.concatenate(
-            [self.spectrogram, spectrum[None]])
-        self.spectrum_stamp.append(msg.header.stamp)
-        # Extract spectrogram of last (self.spectrogram_period) seconds
-        time_now = rospy.Time.now()
-        for i, stamp in enumerate(self.spectrum_stamp):
-            diff = (time_now - stamp).to_sec()
-            if diff > 0.0 and diff < self.spectrogram_period:
-                self.spectrogram = self.spectrogram[i:]
-                self.spectrum_stamp = self.spectrum_stamp[i:]
-                return
-        self.spectrogram = self.spectrogram[self.spectrogram.shape[0]:]
-        self.spectrum_stamp = []
+        with self.spectrum_lock:
+            # Add spectrum msg to buffer
+            spectrum = np.array(msg.amplitude, dtype=np.float32)
+            self.spectrogram = np.concatenate(
+                [self.spectrogram, spectrum[None]])
+            self.spectrum_stamp.append(msg.header.stamp)
+            # Extract spectrogram of last (self.spectrogram_period) seconds
+            time_now = rospy.Time.now()
+            for i, stamp in enumerate(self.spectrum_stamp):
+                diff = (time_now - stamp).to_sec()
+                if diff > 0.0 and diff < self.spectrogram_period:
+                    self.spectrogram = self.spectrogram[i:]
+                    self.spectrum_stamp = self.spectrum_stamp[i:]
+                    return
+            self.spectrogram = self.spectrogram[self.spectrogram.shape[0]:]
+            self.spectrum_stamp = []
 
     def timer_cb(self, timer):
-        if self.spectrogram.shape[0] == 0:
-            return
-        # Reshape spectrogram
-        spectrogram = self.spectrogram.transpose(1, 0)[::-1, :]
-        spectrogram = cv2.resize(
-            spectrogram, (self.image_width, self.image_height))
-        # Publish spectrogram
-        spectrogram_msg = self.bridge.cv2_to_imgmsg(spectrogram, '32FC1')
-        spectrogram_msg.header.stamp = self.spectrum_stamp[-1]
-        self.pub_spectrogram.publish(spectrogram_msg)
+        with self.spectrum_lock:
+            if self.spectrogram.shape[0] == 0:
+                return
+            # Reshape spectrogram
+            spectrogram = self.spectrogram.transpose(1, 0)[::-1, :]
+            spectrogram = cv2.resize(
+                spectrogram, (self.image_width, self.image_height))
+            # Publish spectrogram
+            spectrogram_msg = self.bridge.cv2_to_imgmsg(spectrogram, '32FC1')
+            spectrogram_msg.header.stamp = self.spectrum_stamp[-1]
+            self.pub_spectrogram.publish(spectrogram_msg)
 
 
 if __name__ == '__main__':
